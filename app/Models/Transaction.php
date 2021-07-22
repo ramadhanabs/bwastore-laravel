df0399e31668009ab579a4079b55daa86b1d1b94
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2021-07-22 14:30:16 +0700

    Ubah model transaction & transaction detail

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -21,4 +21,8 @@ class Transaction extends Model
     protected $hidden = [
 
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -20,4 +20,12 @@ class TransactionDetail extends Model
 
     protected $hidden = [];
 
+    public function product(){
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
+
+    public function transaction(){
+        return $this->hasOne(Transaction::class, 'id', 'transaction_id');
+    }
+
 }
